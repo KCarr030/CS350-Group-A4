79cff77cf667900f51bcd8f45b3c141c5eeef936
Kevin Carr <kcarr030@odu.edu>
2023-03-15 18:11:15 -0400

    Kevin Commit

--- project/src/main/java/edu/odu/cs/cs350/pne/Offering.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Offering.java
@@ -1,1 +1,10 @@
 package edu.odu.cs.cs350.pne;
+
+/** This is the class
+ * @author Group A4
+ * 
+ */
+
+ public class Offering {
+    
+ }
---
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
@@ -0,0 +1,13 @@
+package edu.odu.cs.cs350.pne;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Test;
+
+public class TestOffering {
+
+    
+
+}
