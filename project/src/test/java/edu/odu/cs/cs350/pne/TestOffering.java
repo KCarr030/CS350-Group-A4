a1ec1152917f3a82a5774a65e5e69059a9f988e9
Steven <schen012@odu.edu>
2023-03-16 14:19:26 -0400

    bySchen012

--- project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
@@ -1,42 +1,13 @@
 package edu.odu.cs.cs350.pne;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDate;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.time.LocalDate;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.io.TempDir;
-import static org.hamcrest.MatcherAssert.assertThat; 
-import static org.hamcrest.Matchers.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class TestOffering {
 
-    private Offering offering;
-    private LocalDate date;
-    private String courseName;
-    private String time;
-    private String group;
-    private int seats;
-    private int enrollment;
-    private int maxCap;
-
-    @Test
-    public void setup() {
-        date = LocalDate.of(2023, 3, 16);
-        courseName = "CS 361";
-        time = "10:00-12:00";
-        group = "A";
-        seats = 20;
-        enrollment = 10;
-        maxCap = 30;
-        offering = new Offering(date, courseName, time, group, seats, enrollment, maxCap);
-    }
-    
 
 
 }
