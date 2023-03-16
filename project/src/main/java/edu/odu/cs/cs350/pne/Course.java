8d74468cd3a226d323d7b3371a2c310c6f05776f
Steven <schen012@odu.edu>
2023-03-16 17:44:15 -0400

    schen012

--- project/src/main/java/edu/odu/cs/cs350/pne/Course.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Course.java
@@ -1,5 +1,7 @@
 package edu.odu.cs.cs350.pne;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 /** This is the class
@@ -8,5 +10,29 @@ package edu.odu.cs.cs350.pne;
  */
 
  public class Course {
+      // Course properties
+    private String name;
+    private List<Offering> offerings;
 
+    // Constructor
+        public Course(String name) {
+            this.name = name; 
+            this.offerings = new ArrayList<>(); 
+        }
+
+        // Getters
+        public String getName() {
+            return name;
+        }
+
+        public List<Offering> getOfferings() {
+            return offerings;
+        }
+
+        // Add an offering to the course
+        public void addOffering(Offering offering) {
+            if (!offerings.contains(offering)) {
+                offerings.add(offering); 
+        }
+    }
  }
--- project/src/test/java/edu/odu/cs/cs350/pne/TestCourse.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestCourse.java
@@ -1,4 +1,6 @@
 package edu.odu.cs.cs350.pne;
+
+import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,5 +8,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class TestCourse {
+
+    @Test
+    public void testCourseConstructor(){
+        Course course = new Course("CS 361");
+        assertEquals("CS 361", course.getName());
+        assertNotNull(course.getOfferings());
+        assertTrue(course.getOfferings().isEmpty());
+        assertTrue(course.getOfferings() instanceof ArrayList);
+    }
     
 }
