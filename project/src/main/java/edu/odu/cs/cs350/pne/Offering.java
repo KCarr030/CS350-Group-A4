6cbe8adf1bc5c94149295926263f254caa18620e
Steven <schen012@odu.edu>
2023-03-17 14:28:35 -0400

    schen012 setter and getters

--- project/src/main/java/edu/odu/cs/cs350/pne/Offering.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Offering.java
@@ -14,11 +14,25 @@ import java.time.format.DateTimeParseException;
     private LocalDate startTime;
     private LocalDate endTime;
 
+
+    //Time Range
     //Constructor 
     public Offering(String startDateStr, String endDateStr) throws DateTimeParseException{
         this.startTime = LocalDate.parse(startDateStr);
         this.endTime = LocalDate.parse(endDateStr);
     }
+    public void setDateRange(String startDateStr, String endDateStr) throws DateTimeParseException{
+        LocalDate startDate = LocalDate.parse(startDateStr);
+        LocalDate endDate = LocalDate.parse(endDateStr);
+
+        if (startDate.isAfter(endDate)){
+            throw new IllegalArgumentException("Start date must be before or equal to end date");
+        }
+        this.startTime = startDate;
+        this.endTime = endDate;
+    }
+
+    //getters for start and end
     public LocalDate getStartTime(){
         return startTime;
     }
--- project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
@@ -25,3 +25,4 @@ public class TestOffering {
         assertEquals(LocalDate.parse(endDateStr), offering.getEndTime());
     }
 }
+
