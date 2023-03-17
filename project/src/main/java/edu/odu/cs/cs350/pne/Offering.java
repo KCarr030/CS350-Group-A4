6126b59bd55afebe1e3b6482e0740b96428a5846
Steven <schen012@odu.edu>
2023-03-17 14:13:59 -0400

    schen012

--- project/src/main/java/edu/odu/cs/cs350/pne/Offering.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Offering.java
@@ -1,4 +1,7 @@
 package edu.odu.cs.cs350.pne;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
 
 
 
@@ -8,5 +11,13 @@ package edu.odu.cs.cs350.pne;
  */
 
  public class Offering {
+    private LocalDate startTime;
+    private LocalDate endTime;
 
+    //Constructor 
+    public Offering(String startDateStr, String endDateStr) throws DateTimeParseException{
+        this.startTime = LocalDate.parse(startDateStr);
+        this.endTime = LocalDate.parse(endDateStr);
+    }
+    
  }
--- project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
@@ -3,9 +3,25 @@ package edu.odu.cs.cs350.pne;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class TestOffering {
-    
+    @Test
+    public void testConstructor(){
+        String startDateStr = "2022-08-10";
+        String endDateStr = "2022-12-31";
+        Offering offering = null;
+        try {
+            offering = new Offering(startDateStr, endDateStr);
+        }catch (DateTimeParseException e) {
+            fail("Error.");
+        }
+        assertEquals(LocalDate.parse(startDateStr), offering.getStartTime());
+        assertEquals(LocalDate.parse(endDateStr), offering.getEndTime());
+    }
 }
