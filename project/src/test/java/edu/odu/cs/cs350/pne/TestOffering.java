d54aba1c2bf4fafe9ebcbdf7169a0e579d90be1b
Steven <schen012@odu.edu>
2023-04-04 14:29:13 -0400

    testgetTime

--- project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestOffering.java
@@ -28,8 +28,6 @@ public class TestOffering {
     @Test
     public void testGetCourseName() {
         Offering offering = new Offering("CS361", LocalDate.of(2022, 4, 1), "A", 50, 0, 50, "J.Morris");
-        Offering offering2 = new Offering("CS 361", LocalDate.of(2022, 4, 1), "A", 20, 5, 30, "F.Wang");
-        Offering offering3 = new Offering("CS 361", LocalDate.of(2022, 4, 1), "B", 20, 10, 30, "J.Sun");
         assertEquals("CS361", offering.getCourseName());
     }
 
@@ -37,8 +35,6 @@ public class TestOffering {
     public void testGetTime() {
         LocalDate expectedTime = LocalDate.of(2022, 4, 1);
         Offering offering = new Offering("CS 361", expectedTime, "A", 50, 20, 30, "J.Morris");
-        Offering offering2 = new Offering("CS 361", LocalDate.of(2022, 4, 1), "A", 20, 5, 30, "F.Wang");
-        Offering offering3 = new Offering("CS 361", LocalDate.of(2022, 4, 1), "B", 20, 10, 30, "J.Sun");
         LocalDate actualTime = offering.getTime();
         assertEquals(expectedTime, actualTime);
     }
@@ -59,8 +55,6 @@ public class TestOffering {
     @Test
     public void testGetPercentFilled() {
         Offering offering = new Offering("CS 361", LocalDate.of(2022, 4, 1), "A", 20, 10, 30, "J.Morris");
-        Offering offering3 = new Offering("CS 361", LocalDate.of(2022, 4, 1), "B", 20, 10, 30, "J.Sun");
-        Offering offering2 = new Offering("CS 361", LocalDate.of(2022, 4, 1), "A", 20, 5, 30, "F.Wang");
 
         assertEquals(33.3, offering.getPercentFilled(), 0.1);
     }
