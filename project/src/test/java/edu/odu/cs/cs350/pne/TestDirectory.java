91d23fbbe69d701f76d847d31aa21bc13df1431b
JonahODU <122562909+JonahODU@users.noreply.github.com>
2023-04-05 16:59:45 -0400

    Updated Directory

--- project/src/test/java/edu/odu/cs/cs350/pne/TestDirectory.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestDirectory.java
@@ -3,46 +3,53 @@ package edu.odu.cs.cs350.pne;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class TestDirectory {
 
-
     @Test
     public void testDirectoryConstructor() {
+        System.out.println(new File("src/test/data/History/202010").getAbsolutePath());
         Directory dir = new Directory(
-                "test/data/History/202010");
-        dir.printContents();
+                "src/test/data/History/202010");
+        // dir.printContents();
 
         assertEquals(dir.getFileName(0), "2020-04-01.csv");
-        assertEquals(dir.getFilePath(0), "**/project/test/data/History/202010/2020-04-01.csv");
+        assertEquals(dir.getFilePath(0), new File("src/test/data/History/202010/2020-04-01.csv"));
 
         assertEquals(dir.getFileName(dir.dirSize() - 1), "dates.txt");
-        assertEquals(dir.getFilePath(dir.dirSize() - 1), "**/project/test/data/History/202010/dates.txt");
+        assertEquals(dir.getFilePath(dir.dirSize() - 1), new File("src/test/data/History/202010/dates.txt"));
 
         System.out.println("testDirectoryConstructor: Success");
     }
 
     @Test
     public void testSetDirectory() {
-        Directory dir = new Directory("**/project/test/data/History/202010");
+        Directory dir = new Directory("src/test/data/History/202010");
 
         assertEquals(dir.getFileName(0), "2020-04-01.csv");
-        assertEquals(dir.getFilePath(0), "**/project/test/data/History/202010/2020-04-01.csv");
+        assertEquals(dir.getFilePath(0), new File("src/test/data/History/202010/2020-04-01.csv"));
 
         assertEquals(dir.getFileName(dir.dirSize() - 1), "dates.txt");
-        assertEquals(dir.getFilePath(dir.dirSize() - 1), "**/project/test/data/History/202010/dates.txt");
+        assertEquals(dir.getFilePath(dir.dirSize() - 1), new File("src/test/data/History/202010/dates.txt"));
 
-        dir.setDirectory("**/project/test/data/History/202020");
+        dir.setDirectory("src/test/data/History/202020");
 
         assertEquals(dir.getFileName(0), "2020-10-05.csv");
-        assertEquals(dir.getFilePath(0), "**/project/test/data/History/202020/2020-10-05.csv");
+        assertEquals(dir.getFilePath(0), new File("src/test/data/History/202020/2020-10-05.csv"));
 
-        assertEquals(dir.getFileName(dir.dirSize() - 1), "dates.txt");
-        assertEquals(dir.getFilePath(dir.dirSize() - 1), "**/project/test/data/History/202020/dates.txt");
+        assertEquals(dir.getFileName(dir.dirSize() - 1), "dates.txt~");
+        assertEquals(dir.getFilePath(dir.dirSize() - 1), new File("src/test/data/History/202020/dates.txt~"));
+
+        // System.out.println(dir.datesIndex());
+        assertEquals(dir.datesIndex(), 243);
+        assertEquals(dir.getFileName(dir.datesIndex()), "dates.txt");
 
         System.out.println("testSetDirectory: Success");
     }
-    
+
 }
