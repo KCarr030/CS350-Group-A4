0acf4c9b76e746b55a98b4f29adbd936100a6239
Steven <schen012@odu.edu>
2023-04-05 13:34:31 -0400

    added teh getter in section

--- project/src/main/java/edu/odu/cs/cs350/pne/Section.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Section.java
@@ -14,4 +14,22 @@ public class Section {
         this.capacity = capacity;
         this.enrollment = enrollment;
     }
+    public int getCrn() {
+        return crn;
+    }
+    public String getInstructor() {
+        return instructor;
+    }
+    public String getCourseName() {
+        return courseName;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public int getEnrollment() {
+        return enrollment;
+    }
+    public void setEnrollment(int enrollment) {
+        this.enrollment = enrollment;
+    }
 }
