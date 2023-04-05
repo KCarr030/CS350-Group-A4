bf3c42bd2bdde9c37fed401765e5062ead11182b
Steven <schen012@odu.edu>
2023-04-05 13:33:11 -0400

    adding to section

--- project/src/main/java/edu/odu/cs/cs350/pne/Section.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Section.java
@@ -1,5 +1,17 @@
 package edu.odu.cs.cs350.pne;
 
 public class Section {
-    
+    private int crn;
+    private String instructor;
+    private String courseName;
+    private int capacity;
+    private int enrollment;
+
+    public Section(int crn, String instructor, String courseName, int capacity, int enrollment) {
+        this.crn = crn;
+        this.instructor = instructor;
+        this.courseName = courseName;
+        this.capacity = capacity;
+        this.enrollment = enrollment;
+    }
 }
--- project/src/test/java/edu/odu/cs/cs350/pne/TestSection.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestSection.java
@@ -6,5 +6,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class TestSection {
+    @Test
+void testSectionConstructor() {
+    Section section = new Section(12345, "J.Morris", "CS 361", 50, 25);
+    assertEquals(12345, section.getCrn());
+    assertEquals("J.Morris", section.getInstructor());
+    assertEquals("CS 361", section.getCourseName());
+    assertEquals(50, section.getCapacity());
+    assertEquals(25, section.getEnrollment());
+}
     
 }
