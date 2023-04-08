e450986d0b99a658aa74eeec1ecec589de7d7268
Jonah Peterson <jpete022@odu.edu>
2023-04-08 05:23:35 -0400

    Added date trimming functionality to Directory.java

--- project/src/test/java/edu/odu/cs/cs350/pne/TestDirectory.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestDirectory.java
@@ -52,4 +52,29 @@ public class TestDirectory {
         System.out.println("testSetDirectory: Success");
     }
 
+    @Test
+    public void testSpecifyDates() {
+        Directory dir = new Directory("src/test/data/History/202010");
+
+        dir.trimStartDate("2020-03-01");
+        // This date is before any data,
+        // therefore, no content should be missing
+
+        dir.trimEndDate("2021-01-22");
+        // This should be the same case
+
+        assertEquals(dir.getFileName(0), "2020-04-01.csv");
+        assertEquals(dir.getFilePath(0), new File("src/test/data/History/202010/2020-04-01.csv"));
+        assertEquals(dir.getFileName(dir.dirSize() - 1), "dates.txt");
+        assertEquals(dir.getFilePath(dir.dirSize() - 1), new File("src/test/data/History/202010/dates.txt"));
+
+        dir.trimDates("2020-08-01", "2021-01-01");
+        // By this usage, all dates starting at 2020-08-01 and ending before 2021-01-01
+        // should be included, nothing else
+
+        assertEquals(dir.getFileName(0), "2020-08-01.csv");
+        assertEquals(dir.getFilePath(0), new File("src/test/data/History/202010/2020-08-01.csv"));
+        assertEquals(dir.getFileName(dir.dirSize() - 1), "dates.txt");
+        assertEquals(dir.getFilePath(dir.dirSize() - 1), new File("src/test/data/History/202010/dates.txt"));
+    }
 }
