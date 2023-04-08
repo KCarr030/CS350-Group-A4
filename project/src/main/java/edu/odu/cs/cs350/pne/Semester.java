3c708e53e6af245098b56e169cc4fd80dcc907b7
Nia Newton <nnewt001@odu.edu>
2023-04-08 10:49:57 -0400

    Nia Changes

--- project/src/main/java/edu/odu/cs/cs350/pne/Semester.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Semester.java
@@ -14,95 +14,63 @@ public class Semester
         semestDirct =  Semester Directory 
         RegistDate = Registration Date
         withwDate = Withdraw Date 
-        offering = courses being offered for desired semester --string? 
+        offering = courses being offered for desired semester --string?
         history = previous semester
         current = current semester*/
     
-    // Not great with designing classes, these may need to be some sort of list or other ADT
-    private Offering offering; // changed offering to offering class
-    private Directory semestDirct; // changed directory to directory class, please get back to me about whether this directory is same as directory in directory class
-    private History history; // changed history to history class
+    // Took nolans reccomendation--list -jodi
+    private List<Offering> offering; // changed offering to offering class
+    private List<Directory> semestDirct; // changed directory to directory class, please get back to me about whether this directory is same as directory in directory class
+    private List<History> history; // changed history to history class
 
-    private int registDate, withwDate, current;  // current is int?  Wouldn't current be another semester object? -nolan
-    // Unsure if we would want these values to be ints, these could be type string instead -nolan
+    private String registDate, withwDate, current;  //Current is int?  Wouldn't current be another semester object? -nolan
+    //Waiting for elaboration
    
     
     //Constructor for integers and string variables
-    public Semester(Directory semesterDirct, int registDate, int withwDate, History history, int current, Offering offering)
+    public Semester(Directory semesterDirct, String registDate, String withwDate, History history, String current, Offering offering)
     {
-        this.semestDirct = semesterDirct;
+        
         this.registDate = registDate;
         this.withwDate = withwDate;
-        this.history = history; //will we be connecting the history class?
         this.current = current;
-        this.offering = offering; 
-
     }
 
    
     ///Getters for private variables
-    public int getSemestDirect()
+    public List<Directory> getSemestDirect()
     {
         return semestDirct;
     }
 
-    public int getRegistDate()
+    public String getRegistDate()
     {
         return registDate;
     }
 
-    public int getWithwDate()
+    public String getWithwDate()
     {
         return withwDate;
     }
 
-    public int getHistory()
+    public List<History> getHistory()
     {
        return history;
     }
 
-    public int getCurrent()
+    public String getCurrent()
     {
         return current;
     }
 
-    public String getOffering()
+    public  List<Offering> getOffering()
     {
         return offering;
     }
 
-     /*Setters for prvate varaibles -- Do we need setters? 
-        Or are the constructors the setters*/
+    
 
-     public void setSemestDirect(int nSemestDirect)
-     {
-         this.semestDirct = nSemestDirect;
-     }
- 
-     public void getregistDate(int nRegistDate)
-     {
-        this.registDate = nRegistDate;
-     }
- 
-     public void withwDate(int nWithwDate)
-     {
-         this.withwDate = nWithwDate;
-     }
- 
-     public void history(int nHistory)
-     {
-         this.history = nHistory;
-     }
- 
-     public void current(int nCurrent)
-     {
-         this.current = nCurrent;
-     }
- 
-     public void offering(String nOffering)
-     {
-         this.offering = nOffering;
-     }
+   
      /*
      ******FUNCTION IS UNFINISHED******
      As of creation the classes are not fully developed and exact functions/variables
@@ -129,7 +97,7 @@ public class Semester
                 String[] tempRead = line.split(splitBy); // separate by commas, tempRead temporarily stores all data
                 // there are 31 columns that actually contain data, the position in the array acts as a means of choosing a column
                 // for example, CRN is column 2, assuming Course.java will store CRN we can store crn as
-                // Course.setCrn() = tempRead[1];
+                //Course.setCrn() = tempRead[1];
                 // and do that for each of the entries required
             }
         }
@@ -137,7 +105,6 @@ public class Semester
         {
             e.printStackTrace();
         }
-        
 
      }
 
