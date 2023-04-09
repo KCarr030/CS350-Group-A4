dadaf6646c3e7f10a36e826ef0184212b28ad708
nolan <nmarc001@odu.edu>
2023-04-08 23:16:46 -0400

    readData() implementation, changed directory type

--- project/src/main/java/edu/odu/cs/cs350/pne/Semester.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Semester.java
@@ -1,9 +1,12 @@
 package edu.odu.cs.cs350.pne;
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner; // new class import for reading
+import java.util.Vector;
+import java.util.Collections;
+import java.io.File;
 
 //Where the Semester Data is stored- Semester consists of muliple courses.
 //Semester
@@ -18,13 +21,11 @@ public class Semester
         history = previous semester
         current = current semester*/
     
-    // Took nolans reccomendation--list -jodi
-    private List<Offering> offering; // changed offering to offering class
-    private List<Directory> semestDirct; // changed directory to directory class, please get back to me about whether this directory is same as directory in directory class
-    private List<History> history; // changed history to history class
+    private List<Offering> offering;
+    private Directory semestDirct; // don't know if we needed a list of directory -nolan
+    private List<History> history;
 
-    private String registDate, withwDate, current;  //Current is int?  Wouldn't current be another semester object? -nolan
-    //Waiting for elaboration
+    private String registDate, withwDate, current;
    
     
     //Constructor for integers and string variables
@@ -38,7 +39,7 @@ public class Semester
 
    
     ///Getters for private variables
-    public List<Directory> getSemestDirect()
+    public Directory getSemestDirect()
     {
         return semestDirct;
     }
@@ -71,44 +72,39 @@ public class Semester
     
 
    
-     /*
-     ******FUNCTION IS UNFINISHED******
-     As of creation the classes are not fully developed and exact functions/variables
-     have yet to be decided, so this function will be mainly pseudocode until the classes
-     are more developed
-     */
 
-     public void readData(File input)
+/*
+     public void readData()
      {
-        // Assuming main has asked the user to provide the semester, this function is called by main
-        // is this function reading in from a file like in data/history?
-        // Also, we might make code much easier if we were to look into open source libraries for reading csv's
-        // Following example https://www.javatpoint.com/how-to-read-csv-file-in-java
-        String line = "";
-        String splitBy =',';
+        Vector<File> tempRead = this.semestDirct.files; // private member error, fix in next sprint
 
-        try
+        for(File itr : tempRead)
         {
-            BufferedReader br = new BufferedReader(new FileReader(input));
-            while((line = br.readLine()) != null)
+            try
             {
-                // TEMP STRUCTURE, FIND WHAT INFORMATION IS ACTUALLY NEEDED AND SEE IF WE CAN'T SKIP OVER COLUMNS IF DATA ISN'T NEEDED
-                // FIND OUT IF STRINGS ARE THE BEST FOR READING INPUT (SEE IF STRINGS IS EASILY CONVERTED; STRING -> INT, STRING -> RANDOM DATA TYPE)
-                String[] tempRead = line.split(splitBy); // separate by commas, tempRead temporarily stores all data
-                // there are 31 columns that actually contain data, the position in the array acts as a means of choosing a column
-                // for example, CRN is column 2, assuming Course.java will store CRN we can store crn as
-                //Course.setCrn() = tempRead[1];
-                // and do that for each of the entries required
+                Scanner scanner = new Scanner(itr); // new scanner
+                scanner.nextLine();
+                while(scanner.hasNextLine())
+                {
+                    String curLine = scanner.nextLine();
+                    curLine = curLine.trim();
+                    String[] selected = curLine.split("",""); // splits line into separate strings by comma, causes error
+                    Offering tempOff = new Offering(selected);
+                    offering.add(tempOff);
+                }
+                scanner.close();
+                
+            }
+            catch (FileNotFoundException e) // cannot find symbol error
+            {
+                System.err.printIn("Error in finding files"); // cannot find symbol error
+                System.exit(1);
             }
         }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
-     }
 
      
     }
+    */
+}
 
    
