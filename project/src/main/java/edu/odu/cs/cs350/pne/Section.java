0525a7bdab1e0bc16a5b3a1313e4c8fdb35a6cd7
Steven <schen012@odu.edu>
2023-04-19 15:28:35 -0400

    update in section

--- project/src/main/java/edu/odu/cs/cs350/pne/Section.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Section.java
@@ -7,13 +7,13 @@ import java.util.Comparator;
 
 
 public class Section {
-    private String overallCapacity;
-    private String overallEnrollment;
-    private String ptrmStart;
-    private String ptrmEnd;
-    private String notes;
-    private String comments;
-    private String seats;
+    private String campus;
+    private String print;
+    private String time;
+    private String days;
+    private String building;
+    private String room;
+	private String seats;
     private String CRN;
     private String subject;
     private String course;
@@ -21,28 +21,26 @@ public class Section {
     private String crossListCapacity;
     private String enrollment;
     private String link;
-    private String campus;
-    private String print;
-    private String time;
-    private String days;
-    private String building;
-    private String room;
     private String instructor;
-    private int num;
+    private String overallCapacity;
+    private String overallEnrollment;
+    private String ptrmStart;
+    private String ptrmEnd;
+    private int capacity;
+	private int num;
 
 
-
-
-
-    	
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//This constructor takes an array of Strings as a parameter, which represents the fields of a section in a university course catalog.
+//It then assigns the values of each field to the corresponding instance variables of the Section object being constructed. 
 	public Section(String[] fields) {	
-		this.campus = fields[12];
+        this.campus = fields[12];
         this.print = fields[14];
         this.time = fields[15];
         this.days = fields[16];
         this.building = fields[17];
         this.room = fields[18];
-        this.seats = fields[0];
+		this.seats = String.valueOf(Integer.parseInt(fields[0]));
         this.CRN = fields[1];
         this.subject = fields[2];
         this.course = fields[3];
@@ -57,6 +55,34 @@ public class Section {
         this.ptrmEnd = fields[27];
 	}
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	//This is a constructor for creating a new Section object from an existing Section object. 
+	//This is useful for creating copies of Section objects without altering the original object.
+	public Section(Section section) {
+        this.CRN = section.CRN;
+        this.capacity = section.capacity;
+        this.campus = section.campus;
+        this.print = section.print;
+        this.time = section.time;
+        this.days = section.days;
+        this.building = section.building;
+        this.room = section.room;
+        this.seats = section.seats;
+        this.subject = section.subject;
+        this.course = section.course;
+        this.creditHours = section.creditHours;
+        this.crossListCapacity = section.crossListCapacity;
+        this.enrollment = section.enrollment;
+        this.link = section.link;
+        this.instructor = section.instructor;
+        this.overallCapacity = section.overallCapacity;
+        this.overallEnrollment = section.overallEnrollment;
+        this.ptrmStart = section.ptrmStart;
+        this.ptrmEnd = section.ptrmEnd;
+    }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	//constructor
+	//creates a new Section object with default values for all its instance variables. These default values are empty strings or zero values.
     public Section() {	
 		link = "";
         campus = "";
@@ -77,9 +103,9 @@ public class Section {
         overallEnrollment = "0";
         ptrmStart = "";
         ptrmEnd = "";
-        notes = "";
-        comments = "";
 }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//defining a Comparator is to provide a custom way of sorting and comparing Section objects. 
 
     public static Comparator<Section> SnapCRN = new Comparator<Section>() {
         public int compare(Section s1, Section s2) {
@@ -88,6 +114,10 @@ public class Section {
             return crn1 - crn2;
         }
        };
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//compares two Section objects based on their "num" field, which is an integer representing the course number.
+//The Comparator is defined as an anonymous inner class with the name "SnapNum".
 
     public static Comparator<Section> SnapNum = new Comparator<Section>() {
 		public int compare(Section s1, Section s2) {
@@ -97,91 +127,55 @@ public class Section {
 		   return num1-num2;
 	   }};
 
-	   public static void mathFR(ArrayList<Section> data) {
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//This method, math, calculating enrollment and capacity data for each course listed in the input ArrayList of Section objects, and then printing out the data in a formatted table.
+
+	   public static void math(ArrayList<Section> data) {
 		ArrayList<Section> alpha = new ArrayList<Section>();
 		int sum = 0;
 		int capSum = 0;
+	
 		char spec = ' ';
 	
-		Collections.sort(data, Section.getCRNComparator());
-		Collections.sort(data, Section.getNumComparator());
-	
-		for (int i = 0; i < data.size(); i++) {
-			Section current = data.get(i);
-			if (i + 1 < data.size() && current.getCourse().equals(data.get(i + 1).getCourse())) {
-				sum += current.getEnrollment();
-				if (!current.getCRN().equals(data.get(i + 1).getCRN())) {
-					int currentCap = 0;
-					try {
-						currentCap = Integer.parseInt(current.getCapacity());
-					} catch (NumberFormatException e) {
-						// catch exception in case the cap field is empty or not a number
+		int j = data.size();
+		Collections.sort(data, Section.SnapCRN);
+		Collections.sort(data, Section.SnapNum);
+		for(int i = 0; i < data.size(); i++) {
+			if(i+1 <= j-1) {
+				if(data.get(i).getCourse().equals(data.get(i+1).getCourse())){
+					sum = sum+data.get(i).getEnrollment();
+					if (!Integer.valueOf(data.get(i).getCRN()).equals(Integer.valueOf(data.get(i + 1).getCRN()))){
+						if (data.get(i).getCapacity() == 0) {
+							data.get(i).setCapacity(i);
+						}
+						capSum = capSum + data.get(i).getCapacity();
 					}
-					capSum += currentCap;
 				}
-			} else {
-				// create new section instance with the aggregated data
-				int currentCap = 0;
-				try {
-					currentCap = Integer.parseInt(current.getCapacity());
-				} catch (NumberFormatException e) {
-					// catch exception in case the cap field is empty or not a number
+				if(!data.get(i).getCourse().equals(data.get(i+1).getCourse())) {
+	
+					alpha.add(new Section(data.get(i)));
+					alpha.get(alpha.size()-1).setEnrollment(sum);
+					alpha.get(alpha.size()-1).setCapacity(capSum);
+					sum = 0;
+					capSum = 0;
 				}
-				alpha.add(new Section(new String[]{
-						current.getSeats(),
-						current.getCRN(),
-						current.getSubject(),
-						current.getCourse(),
-						current.getCampus(),
-						current.getDays(),
-						current.getCreditHours(),
-						String.valueOf(sum),
-						current.getLink(),
-						"",
-						current.getInstructor(),
-						"",
-						"",
-						"",
-						"",
-						"",
-						"",
-						"",
-						"",
-						"",
-						"",
-						"",
-						String.valueOf(currentCap),
-						"",
-						String.valueOf(capSum),
-						"",
-						"",
-						current.getPtrmStart(),
-						current.getPtrmEnd(),
-						"",
-						"",
-						current.getBuilding(),
-						"",
-						current.getRoom(),
-						"",
-						"",
-						"",
-						"",
-						"",
-						""
-				}));
-				sum = current.getEnrollment();
-				capSum = currentCap;
+	
 			}
+			else
+				alpha.add(data.get(j-1));
+	
 		}
+		Collections.sort(alpha, Section.SnapNum);
 	
-		Collections.sort(alpha, Section.getNumComparator());
-	
-		for (int i = 0; i < alpha.size(); i++) {
-			System.out.print(spec + alpha.get(i).getCourse() + "\t" + alpha.get(i).getEnrollment() + "\t\t\t\t" + alpha.get(i).getCapacity() + "\n");
+		for(int i = 0; i < alpha.size(); i++) {
+			System.out.print(spec + alpha.get(i).getCourse() + "\t" + alpha.get(i).getEnrollment() + "/" + alpha.get(i).getCapacity() + "\n");
 		}
 	}
 	
-	
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
     public String getCourse() {
         return subject + course;
     }
