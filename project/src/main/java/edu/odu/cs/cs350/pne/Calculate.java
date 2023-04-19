bc5f48cfa6d80d0f2f86ca93032d82b3bc4713e6
Malcolm Banks <mbank010@odu.edu>
2023-04-19 19:12:34 -0400

    starting calculate.java

--- project/src/main/java/edu/odu/cs/cs350/pne/Calculate.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Calculate.java
@@ -9,7 +9,9 @@ package edu.odu.cs.cs350.pne;
  // Class for performing the projection calculations. Will be called in projection.java functions. 
  public class Calculate {
     // Perform interpolation of the input data.
-    public void interpolation() {}
+    public void interpolation() {
+        
+    }
 
 
 
