68d0c3524d29fc6d012a9fa070adb5b1f385adb7
Malcolm Banks <mbank010@odu.edu>
2023-04-20 18:19:31 -0400

    Read test pass!

--- project/src/test/java/edu/odu/cs/cs350/pne/TestRead.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestRead.java
@@ -1,4 +1,5 @@
 package edu.odu.cs.cs350.pne;
+import java.util.ArrayList;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import com.opencsv.exceptions.CsvValidationException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
@@ -15,11 +17,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class TestRead {
+        
 
     @Test
-    public void testCSVRead() throws CsvValidationException, IOException {
-        Read.file = "./2020-04-01.csv";
-        Read.csvReadFunction();
-        assertFalse(Read.sections.isEmpty());
+    public void testCSVRead() throws IOException {
+        
+        Read.file = "src/test/java/edu/odu/cs/cs350/pne/2020-04-01.csv";
+        File f = new File(Read.file);
+        System.out.println("File path: " + f.getAbsolutePath());
+        try {
+            Read.csvReadFunction();
+        } catch (IOException e){
+            fail("File path: " + System.getProperty("user.dir"));
+        }
+        ArrayList<Section> sections2 = Read.sections;
+        assertFalse(sections2.isEmpty());
     }
+    
 }
