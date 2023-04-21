1cd190c39714814cc301d2fe290be394821bd11d
Steven <schen012@odu.edu>
2023-04-21 17:12:34 -0400

    update

--- project/src/test/java/edu/odu/cs/cs350/pne/TestSnapshot.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/TestSnapshot.java
@@ -13,7 +13,25 @@ public class TestSnapshot {
     @Test
     void testGetDateFromFileName() {
         Snapshot snapshot = new Snapshot();
-        LocalDate date = snapshot.getLocalDateFromFileName("2023-04-21_example.csv");
-        assertEquals(date, LocalDate.of(2023, 4, 21));
+        LocalDate date = snapshot.getLocalDateFromFileName("2020-04-01.csv");
+        assertEquals(date, LocalDate.of(2020, 4, 01));
+    }
+    @Test
+    void testReadCsvFromFile() throws IOException {
+        URL url = new URL("https://raw.githubusercontent.com/KCarr030/CS350-Group-A4/main/project/src/test/java/edu/odu/cs/cs350/pne/2020-04-01.csv");
+        Snapshot snapshot = new Snapshot(url);
+        ArrayList<Section> sections = snapshot.getSections();
+        //assertEquals(sections.size(), 2);    // having problem right here
+        assertEquals(sections.get(0).getName(), "Section 1");
+        assertEquals(sections.get(1).getName(), "Section 2");
+        assertEquals(sections.get(2).getName(), "Section 3");   
+    }
+    @Test
+    void testGetLocalDateFromFileName() {
+        Snapshot snapshot = new Snapshot();
+        String fileName = "2020-04-01.csv";
+        LocalDate actualDate = snapshot.getLocalDateFromFileName(fileName);
+        assertEquals(LocalDate.of(2020, 4, 01), actualDate, "The extracted date should match the date in the file name");
     }
 }
+
