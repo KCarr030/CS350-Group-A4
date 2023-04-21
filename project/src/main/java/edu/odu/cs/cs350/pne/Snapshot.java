72aaccd951fd54769fa6c2d496e4c620a0266af7
Steven <schen012@odu.edu>
2023-04-21 18:33:26 -0400

    comment on snapshot

--- project/src/main/java/edu/odu/cs/cs350/pne/Snapshot.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Snapshot.java
@@ -19,49 +19,100 @@ import com.opencsv.exceptions.CsvValidationException;
  * 
  */
 
+ /**
+ * It represents a snapshot of the sections and their details at a particular date.
+ * It can read data from a CSV file or a URL, and create a list of Section objects from the data.
+ * It can also get the date from the name of the CSV file, and retrieve the list of sections and their details.
+ */
+
 
  public class Snapshot {
-    private LocalDate date;
-    private ArrayList<Section> sections = new ArrayList<>();
+    private LocalDate date; // The date of the snapshot
+    private ArrayList<Section> sections = new ArrayList<>(); // A list of sections
 
+ 
+
+     /**
+     * Constructs a new Snapshot object with a null date and an empty list of sections.
+     */
     public Snapshot() {
         this.date = null;
         this.sections = new ArrayList<>();
     }
 
+
+    /**
+     * Constructs a new Snapshot object by reading data from a CSV file.
+     * @param filePath The path to the CSV file.
+     * @throws IOException if there is an error reading the file.
+     */
     public Snapshot(Path filePath) throws IOException {
         Read.file = filePath.toString();
         Read.csvReadFunction();
         sections = Read.sections;
     }
 
+
+     /**
+     * Constructs a new Snapshot object by reading data from a URL.
+     * @param url The URL of the CSV file.
+     * @throws IOException if there is an error reading the file.
+     */
     public Snapshot(URL url) throws IOException {
         Read.file = url.toString();
         Read.csvReadFunction();
         sections = Read.sections;
     }
 
+
+    /**
+     * Returns the date of the snapshot.
+     * @return The date of the snapshot.
+     */
     public LocalDate getDate() {
         return this.date;
     }
 
+
+    /**
+     * Returns the list of sections and their details.
+     * @return The list of sections and their details.
+     */
     public ArrayList<Section> getSections() {
         return sections;
     }
 
+     /**
+     * Sets the list of sections and their details.
+     * @param sections The list of sections and their details.
+     */
     public void setSections(ArrayList<Section> sections) {
         this.sections = sections;
     }
 
+    /**
+     * Gets the date of the snapshot from the name of the CSV file.
+     * @param fileName The name of the CSV file.
+     * @return The date of the snapshot.
+     */
     public LocalDate getLocalDateFromFileName(String fileName) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         return LocalDate.parse(fileName.substring(0, 10), formatter);
     }
 
+    /**
+     * The Read class contains a static method for reading data from a CSV file
+     * and creating Section objects from the data.
+     */
     private static class Read {
         static String file;
         static ArrayList<Section> sections = new ArrayList<>();
 
+        /**
+         * Reads data from a CSV file and creates Section objects from the data.
+         * The Section objects are added to the sections list.
+         * @throws IOException if there is an error reading the file.
+         */
         public static void csvReadFunction() throws IOException {
             BufferedReader br = new BufferedReader(new FileReader(file));
                 String line = "";
@@ -76,6 +127,9 @@ import com.opencsv.exceptions.CsvValidationException;
             }
     }
 
+     /**
+     * A main method for testing the Section class.
+     */
     public static void main(String[] args) {
         // Create a new Section object
         Section section = new Section("Example Section");
