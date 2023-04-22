d8a31bc6fcb3f846245fee9b8ac5ac9e482dcd01
JonahODU <122562909+JonahODU@users.noreply.github.com>
2023-04-22 18:04:05 -0400

    Updated SSBuilder.java

--- project/src/main/java/edu/odu/cs/cs350/pne/SSBuilder.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/SSBuilder.java
@@ -1,6 +1,7 @@
 package edu.odu.cs.cs350.pne;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 //The import of apache is not quite functioning fully as of yet.
 import org.apache.poi.ss.usermodel.Cell;
@@ -8,19 +9,17 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.FileOutputStream;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
 
 public class SSBuilder {
     private XSSFWorkbook workbook;
-    private Map<String, Object[]> data = new TreeMap<String, Object[]>();
+    // private Map<String, Object[]> data = new TreeMap<String, Object[]>(); Unused
+    // in final version
     private XSSFSheet sheet;
     private int rowToWrite;
 
     /**
      * 
-     * @param sheetName The name of the spreadsheet document
+     * @param sheetName The name of the first sheet
      * @param headers   Array of all headers for data recorded
      */
     public SSBuilder(String sheetName, String[] headers) {
@@ -36,6 +35,8 @@ public class SSBuilder {
         }
 
         rowToWrite = 1;
+
+        System.out.println("Built SSBuilder");
     }
 
     /**
@@ -67,7 +68,7 @@ public class SSBuilder {
      * 
      * @param replace Index of the row to be replaced
      */
-    public void replaceRow(int replace) {
+    public void replaceRow(int replace, Object[] data) {
         Row row = sheet.getRow(replace);
         int cellnum = 0;
         for (Object obj : data) {
@@ -91,10 +92,22 @@ public class SSBuilder {
      * Outputs the currently built spreadsheet to a designated destination
      * 
      * @param destination The File indicating the final location to output to
+     * @param docName     The name of the document being made
      */
-    public void outputFile(File destination) {
-        FileOutputStream out = new FileOutputStream(destination);
-        workbook.write(out);
-        out.close();
+    public void outputFile(String destination, String docName) {
+        FileOutputStream out;
+        System.out.println("Enter outputFile");
+        try {
+            File dest = new File(destination);
+            dest.mkdirs();
+            File f = new File(destination + "/" + docName);
+            out = new FileOutputStream(f);
+            workbook.write(out);
+            out.close();
+            System.out.println("Wrote to " + f.getAbsolutePath());
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
